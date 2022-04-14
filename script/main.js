98b28722e12181e0633018139faec123e40d1967
sujin3 <elwkdlsj03@naver.com>
2022-04-14 09:07:00 +0900

    메인페이지 수정

--- script/main.js
+++ script/main.js
@@ -10,3 +10,18 @@ $(document).ready(function(){
 		$('.submenu1').hide();
 	});
 });
+/* $(function () {
+            $("#gnb > li").on({  //on은 그룹이벤트 핸들러
+                click: function(){
+                    //클릭했다.
+                    console.log($(this).text());
+                },
+                mouseenter: function(){
+                    //마우스가 닿았다.
+                    console.log("마우스가 닿았다.");
+                }, 
+                mouseleave: function(){
+                    //마우스가 닿았다가 떠났다.
+                }
+            });
+        }) */
